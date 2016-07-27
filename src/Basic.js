a271d745c667b06a85d2e4a26f3c4204ca7bee91
florentduveau <florent.duveau@gmail.com>
2016-07-27 15:05:29 +0200

    add Basic keys HOC

---
+++ src/Basic.js
@@ -0,0 +1,32 @@
+import React, {PropTypes} from 'react';
+import {BACK} from './keys';
+
+const Keys = React.createClass({
+
+  propTypes: {
+    onBack: PropTypes.func,
+  },
+
+  componentDidMount() {
+    this.callback = ({keyCode}) => {
+      switch (keyCode) {
+        case BACK:
+          this.props.onBack();
+          break;
+        default:
+      }
+    };
+    window.document.addEventListener('keydown', this.callback);
+  },
+
+  componentWillUnmount() {
+    window.document.removeEventListener('keydown', this.callback);
+  },
+
+  render() {
+    return <div id="hoc-keys">{this.props.children}</div>;
+  },
+
+});
+
+export default Keys;
---
+++ test/Basic.spec.js
@@ -0,0 +1,35 @@
+/* eslint no-unused-expressions:0 */
+/* eslint no-unused-vars:0 */
+import React from 'react';
+import Basic from '../src/Basic';
+import {mount} from 'enzyme';
+import sinon from 'sinon';
+import {BACK} from '../src/keys';
+
+function trigger(keyCode) {
+  const evt = document.createEvent('HTMLEvents');
+  evt.initEvent('keydown', false, false);
+  evt.keyCode = keyCode;
+  document.dispatchEvent(evt);
+}
+
+describe('Basic', () => {
+  it('should wrap with div hoc-keys id', () => {
+    const wrapper = mount(<Basic></Basic>);
+    wrapper.find('#hoc-keys').should.have.length(1);
+  });
+  it('should bind back event', () => {
+    const onBackKey = sinon.spy();
+    const wrapper = mount(<Basic onBack={onBackKey}></Basic>);
+    trigger(BACK);
+    onBackKey.should.have.been.calledOnce;
+    wrapper.unmount();
+  });
+  it('should stop listening event on unmount', () => {
+    const onBackKey = sinon.spy();
+    const wrapper = mount(<Basic onBack={onBackKey}></Basic>);
+    wrapper.unmount();
+    trigger(BACK);
+    onBackKey.should.have.not.been.called;
+  });
+});
