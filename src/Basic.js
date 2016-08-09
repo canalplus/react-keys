0e7a5ea6b16a04a4fb535fee880c6017d5003eac
florentduveau <florent.duveau@gmail.com>
2016-08-09 15:10:05 +0200

    add UP and DOWN event to Basic

--- src/Basic.js
+++ src/Basic.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react';
-import { BACK } from './keys';
+import { BACK, DOWN, UP } from './keys';
 
 const Keys = React.createClass({
 
   propTypes: {
     onBack: PropTypes.func,
+    onDown: PropTypes.func,
+    onUp: PropTypes.func,
   },
 
   componentDidMount() {
@@ -12,6 +14,12 @@ const Keys = React.createClass({
       switch (keyCode) {
         case BACK:
           this.props.onBack();
+          break;
+        case UP:
+          this.props.onUp();
+          break;
+        case DOWN:
+          this.props.onDown();
           break;
         default:
       }
