970ae22b1c27747c27a4b2e38f8a74fae01ab052
MathildeCanesse <mathilde.canesse@gmail.com>
2016-09-05 15:47:43 +0200

    Listener updated for long tap.

--- src/listener.js
+++ src/listener.js
@@ -1,23 +1,44 @@
 /* eslint import/no-mutable-exports:0 */
+import {  _updateBinderState } from './redux/actions';
+
 export let keysListeners = [];
 export let globalStore = function() {
 };
+export let fired = false;
+export let pressTimeout = null;
 
 export function cb(e) {
   const keyCode = e.keyCode ? e.keyCode : e;
-  for (const listener of keysListeners) {
-    listener.callback.call(listener.context, keyCode);
+  if (!fired) {
+    fired = true;
+    for (const listener of keysListeners) {
+      pressTimeout = setTimeout(() => {
+        _updateBinderState(listener.context.props.id, { press: fired })
+      }, 1000);
+      listener.callback.call(listener.context, keyCode);
+    }
   }
 }
+
+export function cbRelease() {
+  clearTimeout(pressTimeout);
+  fired = false;
+  for (const listener of keysListeners) {
+    _updateBinderState(listener.context.props.id, { press: fired })
+  }
+}
+
 
 export function _init(ops) {
   globalStore = ops && ops.store ? ops.store : function() {
   };
   if (!ops || (ops && !ops.bindkeys)) {
     document.addEventListener('keydown', cb);
+    document.addEventListener('keyup', cbRelease);
   } else {
-    ops.bindkeys(cb);
+    ops.bindkeys(cb, cbRelease);
   }
+
 }
 
 export function addListener(callback, context) {
