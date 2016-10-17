f0b26b22f982f2fe141294aebfa53aca6e270344
Morgane Lecurieux Lafayette <morgane.lecurieux.lafayette@gmail.com>
2016-10-17 17:58:35 +0200

    The activation state is decided also according to the prop

--- src/isActive.js
+++ src/isActive.js
@@ -2,7 +2,7 @@ export function isActive(state, { id, active }) {
   let response = false;
   if (state.getState) {
     response = state.getState()['@@keys'] && state.getState()['@@keys'][id]
-      && state.getState()['@@keys'][id].active;
+      && state.getState()['@@keys'][id].active && active;
   } else {
     response = active;
   }
