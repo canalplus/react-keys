f919d061edd6cf4ff0ce60944c923b34cebc17fa
florentduveau <florent.duveau@gmail.com>
2016-10-18 12:04:19 +0200

    fix active bug

--- src/isActive.js
+++ src/isActive.js
@@ -4,6 +4,6 @@ import { globalStore } from './listener';
 export function isActive({ id, active }) {
   const state = globalStore.getState();
   return state[NAME] && state[NAME][id]
-    ? !!state[NAME][id].active
+    ? !!state[NAME][id].active && !!active
     : !!active;
 }
