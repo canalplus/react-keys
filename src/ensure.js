9c339c2c5d7730afc9ffe21376f98d96d7899c2e
florentduveau <florent.duveau@gmail.com>
2016-11-28 15:14:34 +0100

    add prefix to errors

--- src/ensure.js
+++ src/ensure.js
@@ -1,26 +1,28 @@
 import { globalStore } from './listener';
 import { NAME } from './constants';
 
+const prefix = '[react-keys] - ';
+
 export function ensureState() {
   if (!globalStore.getState()[NAME]) {
-    throw new Error('keys state not present un global state');
+    throw new Error(`${prefix}keys state not present un global state`);
   }
 }
 
 export function ensureDispatch() {
   if (!globalStore.dispatch) {
-    throw new Error('You better have Redux to use this feature');
+    throw new Error(`${prefix}You better have Redux to use this feature`);
   }
 }
 
 export function ensureMountedBinder(binderId) {
   if (!Object.keys(globalStore.getState()[NAME]).some(key => binderId === key)) {
-    throw new Error(`You cannot activate a unmounted binder (${binderId}).`);
+    throw new Error(`${prefix}You cannot activate a unmounted binder (${binderId}).`);
   }
 }
 
 export function ensureUnmountedBinder(binderId) {
   if (Object.keys(globalStore.getState()[NAME]).some(key => binderId === key)) {
-    throw new Error(`Already mounted (${binderId}).`);
+    throw new Error(`${prefix}Already mounted (${binderId}).`);
   }
 }
