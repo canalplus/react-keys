0ab4f154b7274594e647132d9b832852d09037e1
florentduveau <florent.duveau@gmail.com>
2016-12-24 11:11:17 +0100

    helpers refactoring

--- test/helpers.js
+++ test/helpers.js
@@ -1,23 +1,8 @@
-export const keyDown = keyCode => {
-  const evt = document.createEvent("KeyboardEvent");
-  evt.initKeyboardEvent("keydown", true, false, document, '', 0, false, false, false);
-  document.dispatchEvent(Object.defineProperties(evt, {
-    keyCode: {
-      get: function() {
-        return keyCode
-      }
-    },
-  }))
-};
+export const keyDown = keyCode => dispatchAction('keydown', keyCode);
+export const keyUp = keyCode => dispatchAction('keyup', keyCode);
 
-export const keyUp = keyCode => {
-  const evt = document.createEvent("KeyboardEvent");
-  evt.initKeyboardEvent("keyup", true, false, document, '', 0, false, false, false);
-  document.dispatchEvent(Object.defineProperties(evt, {
-    keyCode: {
-      get: function() {
-        return keyCode
-      }
-    },
-  }))
+export const dispatchAction = (action, keyCode) => {
+  const evt = document.createEvent('KeyboardEvent');
+  evt.initKeyboardEvent(action, true, false, document, '', 0, false, false, false);
+  document.dispatchEvent(Object.defineProperty(evt, 'keyCode', { get: () => keyCode }));
 };
