9ffd1674eea0c796a9e9458265c0db3c855ab728
florentduveau <florent.duveau@gmail.com>
2017-11-02 12:31:27 +0100

    [dev] add 1B file

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
-  entry: ['./dev/6B_SCROLL.js'],
+  entry: ['./dev/1B.js'],
   output: {
     path: __dirname,
     filename: 'bundle.js',
